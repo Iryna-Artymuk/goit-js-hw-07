4416d92c43c741d2af5fd6c475e47397a6aa2fc5
Iryna Artymuk <artimyk@ukr.net>
2022-12-10 11:39:20 +0300

    HW7

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -3,3 +3,85 @@ import { galleryItems } from "./gallery-items.js";
 
 // console.log(galleryItems);
 
+const galleryContaiter = document.querySelector(".gallery");
+
+function createdMarkup() {
+  const galleryMarkup = galleryItems
+    .map(
+      (item) =>
+        `<div class="gallery__item">
+        <a class="gallery__link" href="${item.original}">
+          <img
+            class="gallery__image"
+            src= '${item.preview}'
+            data-source= '${item.original}'
+            alt='${item.description}'
+          />
+        </a>
+      </div>`
+    )
+    .join("");
+
+  galleryContaiter.insertAdjacentHTML("beforeend", galleryMarkup);
+}
+
+createdMarkup();
+
+galleryContaiter.addEventListener("click", showLargeImgOnClick);
+;
+function showLargeImgOnClick(event) {
+  event.preventDefault();
+  if (!event.target.classList.contains("gallery__image")) {
+    return  
+  }
+   const modal = basicLightbox
+  .create(
+    `
+    <img width="1400" height="1400" src="${event.target.dataset.source}">
+`
+  )
+  modal.show();
+
+    
+    document.addEventListener("keydown", onKeyPress);
+
+
+    function onKeyPress(event) {
+      
+        if (event.code !== "Escape") {
+          return;
+        } else {
+            modal.close();
+            galleryContaiter.removeEventListener("keydown", showLargeImgOnClick);   
+        
+        }
+    
+      }
+
+}
+
+
+// const onGalleryImgClick = (event) => {
+//   event.preventDefault();
+//   if (event.target.nodeName !== "IMG") {
+//     return;
+//   }
+//   const carousel = basicLightbox.create(
+//     `      <img src="${event.target.dataset.source}" width="800" height="600">  `,
+//     {
+//       onShow: (carousel) => {
+//         document.addEventListener("keydown", onEscapeKeyDown);
+//       },
+//       onClose: (carousel) => {
+//         document.removeEventListener("keydown", onEscapeKeyDown);
+//       },
+//     }
+//   );
+//   carousel.show();
+//   function onEscapeKeyDown(event) {
+//     if (event.code !== "Escape") {
+//       return;
+//     }
+//     carousel.close();
+//   }
+// };
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -3,30 +3,6 @@ import { galleryItems } from './gallery-items.js';
 
 console.log(galleryItems);
  
-const gallery = document.querySelector('.gallery');
 
-
-function creaatMarkup() {
-  const imgMarkup  = galleryItems.map(item =>  
-        
-      `<img src="${item.preview}"alt="${item.description}" class="gallery__image">`).join("")
-    console.log(imgMarkup )
-
-    gallery.insertAdjacentHTML("beforeend", imgMarkup )
-}
-creaatMarkup()
-gallery .addEventListener('click',onClick)
-
-function onClick() {
-    const instance = basicLightbox.create(`
-    <div class="modal">
-        <p>
-            Your first lightbox with just a few lines of code.
-            Yes, it's really that simple.
-        </p>
-    </div>
-    `)  
-    instance.show()
- }
 
 
