0d98d97612df3620ba30177756b024cc477926ef
Iryna Artymuk <artimyk@ukr.net>
2022-12-10 17:33:51 +0200

    task 2 lightbox

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,8 +1,48 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
- 
+// console.log(galleryItems);
+const  galleryContaiter= document.querySelector(".gallery");
+
+function createdMarkup (){
+    const galleryMarkup = galleryItems
+    .map(
+      (item) =>
+     
+        `<a class="gallery__link" href="${item.original}">
+          <img
+            class="gallery__image"
+            src= '${item.preview}'
+            data-source= '${item.original}'
+            alt='${item.description}'
+          />
+        </a>
+      `
+    )
+    .join("");
+
+    galleryContaiter.insertAdjacentHTML("beforeend", galleryMarkup);
 
 
+
+}
+
+createdMarkup ();
+
+galleryContaiter.addEventListener('click',onGalleryItemClick)
+
+function onGalleryItemClick(event){
+    event.preventDefault();
+    console.dir(event.target)
+    console.dir(event.target.alt)
+    var modalBox = new SimpleLightbox('.gallery a', 
+    {overlayOpacity:0.8,
+      captionType	:'att',
+      captionsData: 'alt',
+
+
+
+
+    });
+}
 
